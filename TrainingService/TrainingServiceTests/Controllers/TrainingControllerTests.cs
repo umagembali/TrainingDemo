d31f4ade2dee473e9d57a7ab3607010d94ca52a5
umagembali <48946431+umagembali@users.noreply.github.com>
2019-07-19 00:19:41 +0530

    Test automation methods updated as per action

--- TrainingService/TrainingServiceTests/Controllers/TrainingControllerTests.cs
+++ TrainingService/TrainingServiceTests/Controllers/TrainingControllerTests.cs
@@ -21,10 +21,9 @@ namespace TrainingService.Controllers.Tests
             DateTime startDate = new DateTime(2019, 01, 01);
             DateTime endDate = new DateTime(2019, 02, 01);
 
-            var result = new TrainingController().SaveTraining(trainingName, startDate, endDate);
+            var result = new TrainingController().SavingTrainingRecord(trainingName, startDate, endDate);
             Assert.IsNotNull(result, "Unexpected return code");
-            Assert.IsTrue(((System.Web.Http.Results.OkNegotiatedContentResult<string>)result).Content.ToString().ToLower() == "training has been confirmed for a duration of 32 days. training id is 1");
-            //Assert.IsTrue(((System.Web.Http.Results.OkNegotiatedContentResult<System.Web.Http.Results.JsonResult<string>>)result).Content.Content.ToString().ToLower() == "training has been confirmed for a duration of 32 days. training id is 1");
+            Assert.IsTrue(result.ToString().ToLower() == "training has been confirmed for a duration of 32 days. training id is 1");
         }
 
         [TestMethod()]
@@ -33,21 +32,25 @@ namespace TrainingService.Controllers.Tests
             //Hard coded inputs and also expected message content. Please do change it manually if it is of handful tests. 
             //In case of automation, write code to read the current training id and also duration of training days from dates.
 
-            string trainingName = "VB";
-            DateTime startDate = new DateTime(2019, 01, 01);
-            DateTime endDate = new DateTime(2019, 02, 01);
-
-            var result = new TrainingController().SaveTraining(trainingName, startDate, endDate);
-            Assert.IsNotNull(result, "Unexpected return code");
-            Exception ex = ((System.Web.Http.Results.ExceptionResult)result).Exception;
-            string exMessage = ex.Message + ";";
-            while(ex.InnerException != null)
+            try
             {
-                ex = ex.InnerException;
-                exMessage = exMessage + ex.Message + ";";
-            }
+                string trainingName = "VB";
+                DateTime startDate = new DateTime(2019, 01, 01);
+                DateTime endDate = new DateTime(2019, 02, 01);
 
-            Assert.IsTrue(exMessage.ToLower().Contains("violation of unique key constraint"));
+                var result = new TrainingController().SavingTrainingRecord(trainingName, startDate, endDate);
+                Assert.Fail("Excepted to throw exception");
+            }
+            catch (Exception ex)
+            {
+                string exMessage = string.Format("Error:{0}\r\n", ex.Message);
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    exMessage = string.Format("Error:{0}\r\n", ex.Message);
+                }
+                Assert.IsTrue(exMessage.ToString().ToLower().Contains("violation of unique key constraint"));
+            }
         }
     }
 }
